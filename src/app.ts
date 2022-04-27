72017257603a1a40bed635a1b162e543dc4f1f4e
marlonballa <marlonballa07@gmail.com>
2022-04-27 20:18:37 -0300

    End of authentication process with Github and information retrieval

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import "dotenv/config"; 
 import express, { response } from 'express'; 
+import { router } from "./routes";
+
 
 const app = express();
+app.use(express.json());
 
+app.use(router);
 
 app.get("/github", (req, res) => {
     res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
@@ -10,7 +14,6 @@ app.get("/github", (req, res) => {
 
 app.get("/signin/callback", (req, res) => {
     const { code } = req.query; 
-
     return res.json(code)
 })
 
